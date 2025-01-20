e34ef42a5f542b3cc2e79f5a97ab7de00dc5c24d
Runmonkun Matthew Olawale <runmonkunmatthew@gmail.com>
2025-01-20 16:40:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ async function displayMovieDetails() {
                    alt="${movie.title}"
                    class="movie-img"
                  /> `
-             }
+             
           }
             </div>
 
@@ -279,8 +279,61 @@ async function displaySwiper() {
     const div = document.createElement("div");
     div.classList.add("swiper-slide");
 
-    div.innerHTML = ``;
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              ${
+
+               movie.poster_path
+
+                 ? `
+                 <img
+                   src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+                   alt="${movie.title}"
+                   class="movie-img"
+                 />
+               `
+                 : ` 
+                 <img
+                   src="./images/no-image.jpg"
+                   alt="${movie.title}"
+                   class="movie-img"
+                 /> `
+          }
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed()} / 10
+            </h4>
+    `;
+    
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    
+    initSwiper();
   });
+}
+
+// init slider 
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500:{
+        slidesPerView: 2
+      },
+      700:{
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      },
+    }
+  })
 }
 
 // fetch api data
